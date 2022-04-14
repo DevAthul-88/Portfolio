b5c986a58607649dc83658e0c836f54ce7926226
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-04-14 22:16:57 +0530

    changes

---
+++ app/data/projects.js
@@ -0,0 +1,11 @@
+const projects = [
+ {
+     title:"Stack Overflow Clone",
+     description:"This is a clone web application of a famous Q/A website stack overflow. Built with MERN stack",
+     thumbnail:"https://i.pinimg.com/originals/e7/b9/52/e7b95230907b67ef9b2071c63c1ea6dd.png",
+     link:"#",
+     route:"stackoverflow-clone"
+ }
+]
+
+export default projects;
