69040fcbcbb3bfbbfe45f65a58ed9cf078e0ef68
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-04-14 22:25:56 +0530

    changes

---
+++ app/components/project.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+import project from '../data/projects'
+
+export default class ProjectComponent extends Component {
+   data(){
+       return project
+   }
+}
--- app/data/projects.js
+++ app/data/projects.js
@@ -1,11 +1,13 @@
 const projects = [
- {
-     title:"Stack Overflow Clone",
-     description:"This is a clone web application of a famous Q/A website stack overflow. Built with MERN stack",
-     thumbnail:"https://i.pinimg.com/originals/e7/b9/52/e7b95230907b67ef9b2071c63c1ea6dd.png",
-     link:"#",
-     route:"stackoverflow-clone"
- }
-]
+  {
+    title: 'Stack Overflow Clone',
+    description:
+      'This is a clone web application of a famous Q/A website stack overflow. Built with MERN stack',
+    thumbnail:
+      'https://i.pinimg.com/originals/e7/b9/52/e7b95230907b67ef9b2071c63c1ea6dd.png',
+    link: '#',
+    route: 'stackoverflow-clone',
+  },
+];
 
 export default projects;
