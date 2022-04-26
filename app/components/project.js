74ee15bb15a3ad26082b4432af9f58b8f53c115a
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-04-26 23:09:16 +0530

    changes

--- app/components/project.js
+++ app/components/project.js
@@ -1,7 +1,6 @@
 import Component from '@glimmer/component';
-import project from '../data/projects'
-
+import project from '../data/projects';
 
 export default class ProjectComponent extends Component {
-   data = project
+  data = project;
 }
