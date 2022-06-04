9d1e327d6c6d359eb67543706091e97c9888a566
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-06-04 12:31:16 +0530

    changes

--- app/components/project.js
+++ app/components/project.js
@@ -1,28 +1,25 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import project from '../data/projects';
-import {action} from '@ember/object'
+import { action } from '@ember/object';
 
 export default class ProjectComponent extends Component {
   data = project;
-  @tracked tag = "all";
+  @tracked tag = 'all';
 
-  @action update(args){
-    this.tag = args
+  @action update(args) {
+    this.tag = args;
   }
 
-  get returnData(){
-    if(this.tag === "all"){
-      return this.data = project
-    }
-    else if(this.tag === "e-commerce"){
-      return this.data = project.filter((e) => e.tag === "e-commerce")
-    }
-    else if(this.tag === "system"){
-      return this.data = project.filter((e) => e.tag === "system")
-    }
-    else if(this.tag === "social-network"){
-      return this.data = project.filter((e) => e.tag === "social-network")
+  get returnData() {
+    if (this.tag === 'all') {
+      return (this.data = project);
+    } else if (this.tag === 'e-commerce') {
+      return (this.data = project.filter((e) => e.tag === 'e-commerce'));
+    } else if (this.tag === 'system') {
+      return (this.data = project.filter((e) => e.tag === 'system'));
+    } else if (this.tag === 'social-network') {
+      return (this.data = project.filter((e) => e.tag === 'social-network'));
     }
   }
 }
