84a8cc3791fe47401de1ad676369c4e77f279ee5
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-06-21 19:15:27 +0530

    changes

--- app/data/projects.js
+++ app/data/projects.js
@@ -1,11 +1,23 @@
 const projects = [
   {
     title: 'Stack Overflow Clone',
+    route: 1,
     description:
-      'This is a clone web application of a famous Q/A website stack overflow. Built with MERN stack',
+      'This is a clone web application of a famous Q/A website stack overflow. Built with MERN stack.',
     thumbnail:
       'https://camo.githubusercontent.com/23065d817c377cb163f5caaa7995578e364a7c75adaeef00314e29425451caa4/68747470733a2f2f692e706f7374696d672e63632f38433276317767682f53637265656e73686f742e706e67',
     tag: 'clones',
+    repo: 'https://github.com/DevAthul-88/Stack-Overflow-Clone',
+    live: 'https://stackoverflows-clone.herokuapp.com/',
+    tools: [
+      'React',
+      'Bulma',
+      'Node',
+      'Express',
+      'MongoDB',
+      'Formik+Yup',
+      'Redux',
+    ],
   },
 ];
 
