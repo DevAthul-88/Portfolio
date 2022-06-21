d1f5db7efa0b739dffc65c52bc7d7920dad5a0a2
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-06-21 20:35:35 +0530

    changes

--- app/routes/projects/1.js
+++ app/routes/projects/1.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
-
-export default class Projects1Route extends Route {}
+import data from '../../data/projects'
+export default class Projects1Route extends Route {
+    model(){
+        return data.find((e) => e.route == 1)
+    }
+}
