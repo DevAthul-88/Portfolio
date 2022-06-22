c00c5d221c85a067a7c98eaec212924d221291eb
Athul Vinod <80886537+DevAthul-88@users.noreply.github.com>
2022-06-22 17:38:45 +0530

    more

--- app/routes/projects/5.js
+++ app/routes/projects/5.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import data from '../../data/projects';
 export default class Projects5Route extends Route {
     model() {
-        return data.find((e) => e.route == 4);
+        return data.find((e) => e.route == 5);
       }
       activate() {
         super.activate(...arguments);
